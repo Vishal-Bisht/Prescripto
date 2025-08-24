71bdd950a47887a412b74bc2960d4b01acdea377
Vishal Bisht <bishtvishal390@gmail.com>
2025-08-24 12:48:39 +0530

    admin login and add doctors api created

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,14 +1,14 @@
 import validator from 'validator'
 import bcrypt from 'bcrypt'
 import {v2 as cloudinary} from 'cloudinary'
-import doctorModel from '../models/doctorModel'
-
+import doctorModel from '../models/doctorModel.js'
+import jwt from 'jsonwebtoken'
 
 // API for admin doctor
 const addDoctor = async (req, res) => {
     try {
         const {name, email, password, speciality, degree, experience, about, fees, address} = req.body
-        const imageFile = req.imageFile
+        const imageFile = req.file
         
         // checking for all data to add doctor
         if(!name || !email || !password || !speciality || !degree || !experience || !about || !fees || !address)
@@ -37,13 +37,14 @@ const addDoctor = async (req, res) => {
             password:hashedPassword,
             speciality,
             degree,
+            experience,
             about,
             fees,
             address:JSON.parse(address),
             date:Date.now()
         }
 
-        const newDoctor = new doctorModel(dotorData)
+        const newDoctor = new doctorModel(doctorData)
         await newDoctor.save()
 
         res.json({success:true, message:"Doctor Added"})
@@ -54,4 +55,21 @@ const addDoctor = async (req, res) => {
     }
 }
 
-export {addDoctor}
+// API for admin login
+const loginAdmin = async (req,res) => {
+    try {
+        const {email, password} = req.body
+
+        if(email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD) {
+            const token = jwt.sign(email+password,process.env.JWT_SECRET)
+            res.json({success:true,token})
+        } else {
+            res.json({success:false, message:"Invalid credentials"})
+        }
+    } catch (error) {        
+        console.log(error)
+        res.json({success:false, message:error.message})
+    }
+}
+
+export {addDoctor, loginAdmin}
