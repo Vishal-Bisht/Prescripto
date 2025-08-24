0d7ee21f30a01bbf750d8b00e17bcf73c56fb5f1
Vishal Bisht <bishtvishal390@gmail.com>
2025-08-24 12:47:23 +0530

    assets path updated

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,39 +1,46 @@
-import { assets } from '../assets/frontend/assets'
+import { assets } from "../assets/assets";
 
 const Footer = () => {
   return (
-    <div className='md:mx-10'>
-        <div className='flex flex-col sm:grid grid-cols-[4fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
-            {/* -------left section------- */}
-            <div className='mb-5 w-50'>
-               <img src={assets.logo} alt="prescripto" /> 
-               <p className='w-full md:w-5/3 text-gray-600 leading-6'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-            </div>
-            {/* -------center section------- */}
-            <div>
-                <p className='text-xl font-medium mb-5'>COMPANY</p>
-                <ul className='flex flex-col gap-2 text-gray-600'>
-                    <li>Home</li>
-                    <li>About us</li>
-                    <li>Contact us</li>
-                    <li>Privacy policy</li>
-                </ul>
-            </div>
-            {/* -------right section------- */}
-            <div>
-                <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
-                <ul className='flex flex-col gap-2 text-gray-600'>
-                    <li>+1-212-456-7890</li>
-                    <li>bishtvishal390@gmail.com</li>
-                </ul>
-            </div>
+    <div className="md:mx-10">
+      <div className="flex flex-col sm:grid grid-cols-[4fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
+        {/* -------left section------- */}
+        <div className="mb-5 w-50">
+          <img src={assets.logo} alt="prescripto" />
+          <p className="w-full md:w-5/3 text-gray-600 leading-6">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book.
+          </p>
         </div>
-            {/* -------Copyright Text */}
+        {/* -------center section------- */}
         <div>
-            <hr />
-            <p className='py-5 text-sm text-center'>Copyright 2025@ Prescripto - All Right Reserved</p>
+          <p className="text-xl font-medium mb-5">COMPANY</p>
+          <ul className="flex flex-col gap-2 text-gray-600">
+            <li>Home</li>
+            <li>About us</li>
+            <li>Contact us</li>
+            <li>Privacy policy</li>
+          </ul>
         </div>
+        {/* -------right section------- */}
+        <div>
+          <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
+          <ul className="flex flex-col gap-2 text-gray-600">
+            <li>+1-212-456-7890</li>
+            <li>bishtvishal390@gmail.com</li>
+          </ul>
+        </div>
+      </div>
+      {/* -------Copyright Text */}
+      <div>
+        <hr />
+        <p className="py-5 text-sm text-center">
+          Copyright 2025@ Prescripto - All Right Reserved
+        </p>
+      </div>
     </div>
-  )
-}
-export default Footer
+  );
+};
+export default Footer;
--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,23 +1,20 @@
-import { createContext } from 'react';
-import { doctors } from '../assets/frontend/assets';
-import { useNavigate } from 'react-router-dom';
+import { createContext } from "react";
+import { doctors } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
 
 export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
+  const currencySymbol = "$";
 
-    const currencySymbol = '$'
+  const value = {
+    doctors,
+    currencySymbol,
+  };
 
-    const value = {
-        doctors, 
-        currencySymbol
-    }
-    
-    return (
-        <AppContext.Provider value={value}>
-            {props.children}
-        </AppContext.Provider>
-    )
-}
+  return (
+    <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
+  );
+};
 
 export default AppContextProvider;
