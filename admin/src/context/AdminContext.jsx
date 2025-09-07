7e6956bd96e9de0e70235bd642c2c6523f9955e3
Vishal Bisht <bishtvishal390@gmail.com>
2025-09-07 09:45:51 +0530

    admin dashboarded created

---
+++ admin/src/context/AdminContext.jsx
@@ -0,0 +1,25 @@
+import { createContext, useState } from "react";
+
+export const AdminContext = createContext();
+
+const AdminContextProvider = (props) => {
+  const [aToken, setAToken] = useState(
+    localStorage.getItem("aToken") ? localStorage.getItem("aToken") : ""
+  );
+
+  const backendurl = import.meta.env.VITE_BACKEND_URL;
+
+  const value = {
+    aToken,
+    setAToken,
+    backendurl,
+  };
+
+  return (
+    <AdminContext.Provider value={value}>
+      {props.children}
+    </AdminContext.Provider>
+  );
+};
+
+export default AdminContextProvider;
