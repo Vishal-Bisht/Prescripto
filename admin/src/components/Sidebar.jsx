cda5598357edbbeab83e7834be0d6c10edf2189b
Vishal Bisht <bishtvishal390@gmail.com>
2025-12-25 02:18:28 +0530

    doctor dashboard done

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -2,9 +2,11 @@ import { useContext } from "react";
 import { AdminContext } from "../context/AdminContext";
 import { assets } from "../assets/assets";
 import { NavLink } from "react-router-dom";
+import { DoctorContext } from "../context/DoctorContext";
 
 const Sidebar = () => {
   const { aToken } = useContext(AdminContext);
+  const { dToken } = useContext(DoctorContext);
 
   return (
     <div className="min-h-screen bg-white border-r">
@@ -19,7 +21,7 @@ const Sidebar = () => {
             to={"/admin-dashboard"}
           >
             <img src={assets.home_icon} alt="" />
-            <p>Dashboard</p>
+            <p className="hidden md:block">Dashboard</p>
           </NavLink>
 
           <NavLink
@@ -31,7 +33,7 @@ const Sidebar = () => {
             to={"./all-appointments"}
           >
             <img src={assets.appointment_icon} alt="" />
-            <p>Appointments</p>
+            <p className="hidden md:block">Appointments</p>
           </NavLink>
 
           <NavLink
@@ -43,7 +45,7 @@ const Sidebar = () => {
             to={"./add-doctor"}
           >
             <img src={assets.add_icon} alt="" />
-            <p>Add Doctor</p>
+            <p className="hidden md:block">Add Doctor</p>
           </NavLink>
 
           <NavLink
@@ -52,10 +54,48 @@ const Sidebar = () => {
                 isActive ? "bg-[#F2F3FF] border-r-4 border-[#5F5FFF]" : ""
               }`
             }
-            to={"./doctors-list"}
+            to={"./doctor-list"}
           >
             <img src={assets.people_icon} alt="" />
-            <p>Doctors List</p>
+            <p className="hidden md:block">Doctors List</p>
+          </NavLink>
+        </ul>
+      )}
+      {dToken && (
+        <ul className="text-[#515151] mt-5">
+          <NavLink
+            className={({ isActive }) =>
+              `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${
+                isActive ? "bg-[#F2F3FF] border-r-4 border-[#5F5FFF]" : ""
+              }`
+            }
+            to={"/doctor-dashboard"}
+          >
+            <img src={assets.home_icon} alt="" />
+            <p className="hidden md:block">Dashboard</p>
+          </NavLink>
+
+          <NavLink
+            className={({ isActive }) =>
+              `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${
+                isActive ? "bg-[#F2F3FF] border-r-4 border-[#5F5FFF]" : ""
+              }`
+            }
+            to={"./doctor-appointments"}
+          >
+            <img src={assets.appointment_icon} alt="" />
+            <p className="hidden md:block">Appointments</p>
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${
+                isActive ? "bg-[#F2F3FF] border-r-4 border-[#5F5FFF]" : ""
+              }`
+            }
+            to={"./doctor-profile"}
+          >
+            <img src={assets.people_icon} alt="" />
+            <p className="hidden md:block">Doctor Profile</p>
           </NavLink>
         </ul>
       )}
--- admin/src/pages/Doctor/DoctorDashboard.jsx
+++ admin/src/pages/Doctor/DoctorDashboard.jsx
@@ -1,31 +1,36 @@
 import { useContext, useEffect } from "react";
-import { AdminContext } from "../../context/AdminContext";
+import { DoctorContext } from "../../context/DoctorContext";
 import { assets } from "../../assets/assets";
 import { AppContext } from "../../context/AppContext";
 
-const Dashboard = () => {
-  const { aToken, getDashData, dashData, cancelAppointment } =
-    useContext(AdminContext);
-
-  const { slotDateFormat } = useContext(AppContext);
+const DoctorDashboard = () => {
+  const {
+    dashData,
+    dToken,
+    getDashData,
+    cancelAppointment,
+    completeAppointment,
+  } = useContext(DoctorContext);
+  const { currency, slotDateFormat } = useContext(AppContext);
 
   useEffect(() => {
-    if (aToken) {
+    if (dToken) {
       getDashData();
     }
-  }, [aToken]);
+  }, [dToken]);
 
   return (
     dashData && (
       <div className="m-5">
         <div className="flex flex-wrap gap-3">
           <div className="flex items-center gap-2 bg-white p-4 min-w-52 rounded border-2 border-gray-100 cursor-pointer hover:scale-105 transition-all">
-            <img className="w-14" src={assets.doctor_icon} alt="" />
+            <img className="w-14" src={assets.earning_icon} alt="" />
             <div>
               <p className="text-xl font-semibold text-gray-600">
-                {dashData.doctors}
+                {currency}
+                {dashData.earnings}
               </p>
-              <p className="text-gray-400">Doctors</p>
+              <p className="text-gray-400">Earnings</p>
             </div>
           </div>
           <div className="flex items-center gap-2 bg-white p-4 min-w-52 rounded border-2 border-gray-100 cursor-pointer hover:scale-105 transition-all">
@@ -53,19 +58,19 @@ const Dashboard = () => {
             <p className="font-semibold">Latest Bookings</p>
           </div>
           <div className="pt-4 border border-t-0">
-            {dashData.latestAppointments?.map((item, index) => (
+            {dashData.latestAppointments.map((item, index) => (
               <div
                 className="flex items-center px-6 py-3 gap-3 hover:bg-gray-100"
                 key={index}
               >
                 <img
                   className="rounded-full w-10"
-                  src={item.docData.image}
+                  src={item.userData.image}
                   alt=""
                 />
                 <div className="flex-1 text-sm">
                   <p className="text-gray-800 font-medium">
-                    {item.docData.name}
+                    {item.userData.name}
                   </p>
                   <p className="text-gray-600">
                     {slotDateFormat(item.slotDate)}
@@ -74,16 +79,22 @@ const Dashboard = () => {
                 {item.cancelled ? (
                   <p className="text-red-400 text-xs font-medium">Cancelled</p>
                 ) : item.isCompleted ? (
-                  <p className="text-green-400 text-xs font-medium">
-                    Completed
-                  </p>
+                  <p className="text-green-500 text-xs font-medium">Booked</p>
                 ) : (
-                  <img
-                    onClick={() => cancelAppointment(item._id)}
-                    className="w-10 cursor-pointer"
-                    src={assets.cancel_icon}
-                    alt=""
-                  />
+                  <div className="flex">
+                    <img
+                      onClick={() => cancelAppointment(item._id)}
+                      className="w-10 cursor-pointer"
+                      src={assets.cancel_icon}
+                      alt=""
+                    />
+                    <img
+                      onClick={() => completeAppointment(item._id)}
+                      className="w-10 cursor-pointer"
+                      src={assets.tick_icon}
+                      alt=""
+                    />
+                  </div>
                 )}
               </div>
             ))}
@@ -93,4 +104,4 @@ const Dashboard = () => {
     )
   );
 };
-export default Dashboard;
+export default DoctorDashboard;
